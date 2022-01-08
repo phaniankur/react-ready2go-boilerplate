36664fb0185d2d9bed297b84ba1abaf245ec890e
Ankur Phani <ankur@the-algo.com>
2022-01-08 11:59:28 +0530

    boilerplate commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import { HashRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+      // <Router>
+      //   <Route />
+      // </Router>
+      <h1 className='App'>Start Here</h1>
+
+  );
+}
+
+export default App;
